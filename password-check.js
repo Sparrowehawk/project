0ad2c5e19070bef7573b05a3fb9a9e7f89bd737f
Sparrowe <hamidjamal@hotmail.co.uk>
2023-06-06 00:52:57 +0100

    end of day

---
+++ password-check.js
@@ -0,0 +1,9 @@
+function handlePassword(){
+    var password = prompt("What is da password?");
+    if(password == "Vamos"){
+        alert("pass");
+        window.location.href = "main.html";
+    } else{
+        alert("fail");
+    }
+}
