573a74f2eddc88e0f7de165cd586e14e45881fc9
Sparrowe <hamidjamal@hotmail.co.uk>
2023-06-06 01:02:11 +0100

    modified:   password-check.js

--- password-check.js
+++ password-check.js
@@ -1,8 +1,8 @@
 function handlePassword(){
     var password = prompt("What is da password?");
     if(password == "Vamos"){
-        alert("pass");
-        sessionStorage.setItem("isLoggedIn", "true");
+        alert("pass!");
+        sessionStorage.setItem("isLoggesdIn", "true");
         window.location.href = "main.html";
     } else{
         alert("fail");
