8d1ca74d145f49ed17158ab01e156e662db0785a
Sparrowe <hamidjamal@hotmail.co.uk>
2023-06-06 01:10:46 +0100

    am idiot

--- password-check.js
+++ password-check.js
@@ -2,7 +2,7 @@ function handlePassword(){
     var password = prompt("What is da password?");
     if(password == "Vamos"){
         alert("pass!");
-        sessionStorage.setItem("isLoggesdIn", "true");
+        sessionStorage.setItem("isLoggedIn", "true");
         window.location.href = "main.html";
     } else{
         alert("fail");
